481bc7d7e65ff130603b7cfc0578abb249462af4
jarv1ssa <emin.aliyev@stu.khas.edu.tr>
2022-02-28 03:22:18 +0400

    ✨ create the navigation drawer

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,13 @@
 const iconInputs = document.querySelectorAll(".icon-input");
+
 const searchMobile = document.querySelector(".search-mobile");
 const searchBtn = document.querySelector(".searchBtn");
 const closeMobileSearch = document.querySelector("#closeMobileSearch");
+
+const headerMenu = document.querySelector(".header__menu");
+const aside = document.querySelector(".aside");
+
+const overlay = document.querySelector(".overlay");
 
 iconInputs.forEach((parent) => {
   ["focus", "blur"].forEach((event) => {
@@ -18,4 +24,14 @@ searchBtn.addEventListener("click", () => {
 
 closeMobileSearch.addEventListener("click", () => {
   searchMobile.style.display = "none";
+});
+
+headerMenu.addEventListener("click", () => {
+  aside.style.display = "block";
+  overlay.style.display = "block";
+});
+
+overlay.addEventListener("click", () => {
+  aside.style.display = "none";
+  overlay.style.display = "none";
 });
