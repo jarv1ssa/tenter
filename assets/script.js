af55e33bec4891e033794be771e5473f720333f0
jarv1ssa <emin.aliyev@stu.khas.edu.tr>
2022-03-05 02:11:28 +0400

    ✨ create code & annotation pages

--- assets/script.js
+++ assets/script.js
@@ -9,24 +9,6 @@ const aside = document.querySelector(".aside");
 
 const overlay = document.querySelector(".overlay");
 
-const timer = new easytimer.Timer();
-const days = document.querySelector("#days");
-const hours = document.querySelector("#hours");
-const minutes = document.querySelector("#minutes");
-const seconds = document.querySelector("#seconds");
-
-timer.start({
-  countdown: true,
-  startValues: { days: 1, hours: 4, minutes: 51, seconds: 1 },
-});
-
-timer.addEventListener("secondsUpdated", () => {
-  days.innerText = timer.getTimeValues().days;
-  hours.innerText = timer.getTimeValues().hours;
-  minutes.innerText = timer.getTimeValues().minutes;
-  seconds.innerText = timer.getTimeValues().seconds;
-});
-
 iconInputs.forEach((parent) => {
   ["focus", "blur"].forEach((event) => {
     parent.children[1].addEventListener(event, () => {
